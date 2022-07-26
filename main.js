615ff7dba67f86e18833489d79ede8eae0f81c7e
vothanhan777 <vothanhan777@gmail.com>
2022-07-26 23:57:28 +0700

    js buoi 6

--- main.js
+++ main.js
@@ -1,98 +1,70 @@
-// Bài tập 1: Chương trình Quản lý tuyển sinh
-function tinhDiemUuTien(x, y) {
-  var diemUuTien = 0;
-  if (x != "X") {
-    if (x == "KV1") {
-      diemUuTien += 2;
-    } else if (x == "KV2") {
-      diemUuTien += 1;
-    } else {
-      diemUuTien += 0.5;
-    }
+//Bài tập 1: Tìm số nguyên dương nhỏ nhất sao cho: 1 + 2 + … + n > 10000
+function timSoNguyen() {
+  var sum = 0;
+  var i = 1;
+  while (sum <= 10000) {
+    sum += i;
+    i++;
   }
-  if (y != "0") {
-    if (y == "1") {
-      diemUuTien += 2.5;
-    } else if (y == "2") {
-      diemUuTien += 1.5;
-    } else {
-      diemUuTien += 1;
-    }
-  }
-  return diemUuTien;
+  i--;
+  return i;
 }
-function tinhDiem(a, b, c) {
-  var diem3Mon = a + b + c;
-  return diem3Mon;
-}
-document.getElementById("btnThongBao").onclick = function () {
-  var diemChuan = document.getElementById("diemChuan").value * 1;
-  var diem1 = document.getElementById("diem1").value * 1;
-  var diem2 = document.getElementById("diem2").value * 1;
-  var diem3 = document.getElementById("diem3").value * 1;
-
-  var khuVuc = document.getElementById("khuVuc").value;
-  var uuTien = document.getElementById("uuTien").value;
-
-  var output = "";
-  var tongDiem = tinhDiem(diem1, diem2, diem3) + tinhDiemUuTien(khuVuc, uuTien);
-  output += "Tổng điểm: " + tongDiem + " - ";
-  if (tongDiem >= diemChuan && diem1 > 0 && diem2 > 0 && diem3 > 0) {
-    output += "Đậu";
-  } else {
-    output += "Rớt";
-  }
-
-  document.getElementById("thongBaoKetQua").innerHTML = output;
-  document.getElementById("thongBaoKetQua").classList.add("bg-success");
-  document.getElementById("thongBaoKetQua").classList.add("text-white");
+document.querySelector("#btnTimSo").onclick = function () {
+  var output = "Số Nguyên đó là: " + timSoNguyen();
+  document.querySelector("#thongBaoSoNguyen").innerHTML = output;
 };
 
-//Bài tập 2: Chương trình Tính tiền điện
-function tinhTienDien(luongDien) {
-  const MUC1 = 50;
-  const MUC2 = 100;
-  const MUC3 = 150;
-  const MUC4 = 200;
-
-  const MUCGIA1 = 500;
-  const MUCGIA2 = 650;
-  const MUCGIA3 = 850;
-  const MUCGIA4 = 1100;
-  const MUCGIAKHAC = 1300;
-
-  var tienDien = 0;
-
-  if (luongDien <= MUC1) {
-    tienDien += luongDien * MUCGIA1;
+//Bài tập 2: Viết chương trình nhập vào 2 số x, n tính tổng: S(n) = x +
+// x^2 + x^3 + … + x^n
+function tinhTong(x, n) {
+  var sum = 0;
+  if (n > 1) {
+    for (var i = 1; i <= n; i++) {
+      sum += Math.pow(x, i);
+    }
   } else {
-    tienDien += MUC1 * MUCGIA1;
-    if (luongDien <= MUC2) {
-      tienDien += (luongDien - MUC1) * MUCGIA2;
+    sum = x;
+  }
+  return sum;
+}
+
+document.querySelector("#btnTinhTong").onclick = function () {
+  var x = document.querySelector("#soX").value * 1;
+  var n = document.querySelector("#soN").value * 1;
+
+  var sum = tinhTong(x, n);
+  document.querySelector("#hienThiKQ").innerHTML = sum;
+};
+
+//Bài tập 3: Nhập vào n. Tính giai thừa 1*2*...n
+function tinhGiaiThua(n) {
+  if (n <= 0) {
+    alert("Vui lòng nhập n lớn hơn 0");
+    return;
+  }
+  var result = 1;
+  for (var i = 1; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
+document.querySelector("#btnTinhGiaiThua").onclick = function () {
+  var n = document.querySelector("#numberN").value * 1;
+  document.querySelector("#display").innerHTML = tinhGiaiThua(n);
+};
+
+//Bài tập 4: chương trình khi click vào button sẽ in ra 10 thẻ div. Nếu
+//div nào vị trí chẵn thì background màu đỏ và lẻ thì background màu xanh.
+document.querySelector("#btnInTheDiv").onclick = function () {
+  var listDivs = "";
+  for (var i = 1; i <= 10; i++) {
+    if (i % 2 === 1) {
+      listDivs +=
+        "<div class='bg-primary' style='height: 20px; width: 100%'>" + "</div>";
     } else {
-      tienDien += (MUC2 - MUC1) * MUCGIA2;
-      if (luongDien <= MUC3) {
-        tienDien += (luongDien - MUC2) * MUCGIA3;
-      } else {
-        tienDien += (MUC3 - MUC2) * MUCGIA3;
-        if (luongDien <= MUC4) {
-          tienDien += (luongDien - MUC3) * MUCGIA4;
-        } else {
-          tienDien += (MUC4 - MUC3) * MUCGIA4;
-          tienDien += (luongDien - MUC4) * MUCGIAKHAC;
-        }
-      }
+      listDivs +=
+        "<div class='bg-danger' style='height: 20px; width: 100%'>" + "</div>";
     }
   }
-  return tienDien;
-}
-document.getElementById("btnTinhTienDien").onclick = function () {
-  var luongDienTieuThu = document.getElementById("luongDienTieuThu").value * 1;
-  var soTienDien = tinhTienDien(luongDienTieuThu);
-
-  var output = "Số tiền điện: " + soTienDien;
-
-  document.getElementById("thongBaoSoTienDien").innerHTML = output;
-  document.getElementById("thongBaoSoTienDien").classList.add("bg-success");
-  document.getElementById("thongBaoSoTienDien").classList.add("text-white");
+  document.getElementById("inThe").innerHTML = listDivs;
 };
